e32337b342e79ffb3e04456a61a1c8f5d81034e6
Bot-Arpit <myselfarpitkumar@gmail.com>
2024-02-25 05:09:15 +0530

    intial commit

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,16 @@ module.exports = defineConfig({
   
   e2e: {
     baseUrl : "https://reqres.in/",
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'Backend',
+      embeddedScreenshots: true,
+      inlineAssets: false,
+      saveAllAttempts: false,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
